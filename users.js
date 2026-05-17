d76e700f830d53762cfebb5ba1a80c872d1cd2d6
kwaowu7442 <kwaowu7442@students.ecpi.edu>
2026-05-17 15:21:22 -0400

    Add files via upload

---
+++ users.js
@@ -0,0 +1,18 @@
+router.get('/', function (req, res, next) {
+
+  var places = [
+    { name: 'Home', rating: 10 },
+    { name: 'Beach', rating: 9 },
+    { name: 'Accra Mall', rating: 8 },
+    { name: 'University', rating: 7 },
+    { name: 'Library', rating: 8 }
+  ];
+
+  var myname = "Kwadwo Owusu";
+
+  res.render('index', {
+    title: 'Favorite Places',
+    places: places,
+    myname: myname
+  });
+});
